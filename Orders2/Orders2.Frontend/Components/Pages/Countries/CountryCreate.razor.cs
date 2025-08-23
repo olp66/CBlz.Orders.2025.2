ccfa97afe97b812befd67b61717ac371834db5c1
olp66 <jima6608@gmail.com>
2025-08-23 11:31:11 -0400

    Countries CRUD

--- Orders2/Orders2.Frontend/Components/Pages/Countries/CountryCreate.razor.cs
+++ Orders2/Orders2.Frontend/Components/Pages/Countries/CountryCreate.razor.cs
@@ -1,5 +1,34 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Orders2.Frontend.Repositories;
+using Orders2.Shared.Entities;
+
 namespace Orders2.Frontend.Components.Pages.Countries;
 
 public partial class CountryCreate
 {
+    private Country country = new();
+
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+
+    private async Task CreateAsync()
+    {
+        var responseHttp = await Repository.PostAsync("/api/countries", country);
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(message!, Severity.Error);
+            return;
+        }
+
+        Return();
+        Snackbar.Add("Registro creado", Severity.Success);
+    }
+
+    private void Return()
+    {
+        NavigationManager.NavigateTo("/countries");
+    }
 }
